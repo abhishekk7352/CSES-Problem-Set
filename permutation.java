6314b26e2bc66f56090e1792c7844d4547ec1c0d
Abhishek kumar <63047149+abhishekk7352@users.noreply.github.com>
2022-08-20 19:33:56 +0530

    Update permutation.java

--- permutation.java
+++ permutation.java
@@ -1,21 +1,21 @@
-import java.util.*;
 
-public class permutation
-{
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		long n=sc.nextInt();
-	
-		if(n==1)
-		System.out.println("1");
-		if(n==3|| n==2)
-	System.out.println("NO SOLUTION");
-		int k=0;
-		if(n>=4){
-		    for(long i=2;i<=n;i+=2)
-		    System.out.print(i+" ");
-		    for(long i=1;i<=n;i+=2)
-		    System.out.print(i+" ");
-		}
-	}
+import java.util.*;
+class abhi{
+public static void main(String[] args)  {
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+StringBuilder sb=new StringBuilder();
+if(n==1)
+System.out.println("1");
+else if(n==2 || n==3)
+System.out.println("NO SOLUTION");
+else{
+for(int i=2;i<=n;i+=2)
+sb.append(i+" ");
+for(int i=1;i<=n;i+=2)
+sb.append(i+" ");
+System.out.print(sb);
 }
+}
+}
+
